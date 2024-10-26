66142a1c028027c31523bddeb5adc7ce14258209
metagn <metagngn@gmail.com>
2024-10-26 07:56:06 +0300

    static overloading broken with nim check, remove for now

--- compiler/cbuilderbase.nim
+++ compiler/cbuilderbase.nim
@@ -17,51 +17,16 @@ proc addIntValue(builder: var Builder, val: uint64) =
 proc addIntValue(builder: var Builder, val: Int128) =
   builder.addInt128(val)
 
-template addIntValue(builder: var Builder, val: static int) =
-  builder.add(static($val))
-
-template addIntValue(builder: var Builder, val: static int64) =
-  builder.add(static($val))
-
-template addIntValue(builder: var Builder, val: static uint64) =
-  builder.add(static($val))
-
-proc cIntValue(val: int): Snippet =
-  result = ""
-  result.addInt(val)
-
-proc cIntValue(val: int64): Snippet =
-  result = ""
-  result.addInt(val)
-
-proc cIntValue(val: uint64): Snippet =
-  result = ""
-  result.addInt(val)
-
-proc cIntValue(val: Int128): Snippet =
-  result = ""
-  result.addInt128(val)
-
-template cIntValue(val: static int): Snippet =
-  (static($val))
-
-template cIntValue(val: static int64): Snippet =
-  (static($val))
-
-template cIntValue(val: static uint64): Snippet =
-  (static($val))
+template cIntValue(val: int): Snippet = $val
+template cIntValue(val: int64): Snippet = $val
+template cIntValue(val: uint64): Snippet = $val
+template cIntValue(val: Int128): Snippet = $val
 
 import std/formatfloat
 
 proc addFloatValue(builder: var Builder, val: float) =
   builder.addFloat(val)
 
-template addFloatValue(builder: var Builder, val: static float) =
-  builder.add(static($val))
-
 proc cFloatValue(val: float): Snippet =
   result = ""
   result.addFloat(val)
-
-template cFloatValue(val: static float): Snippet =
-  (static($val))
