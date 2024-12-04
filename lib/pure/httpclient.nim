219c33033f1892cae4aadae856c599c9c611f6d9
ringabout <43030857+ringabout@users.noreply.github.com>
2024-12-04 13:00:31 +0800

    remove unnecessary `await`

--- lib/pure/httpclient.nim
+++ lib/pure/httpclient.nim
@@ -864,7 +864,7 @@ proc parseResponse(client: HttpClient | AsyncHttpClient,
   while true:
     linei = 0
     when client is HttpClient:
-      line = await client.socket.recvLine(client.timeout)
+      line = client.socket.recvLine(client.timeout)
     else:
       line = await client.socket.recvLine()
     if line == "":
