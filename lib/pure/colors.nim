3b938443664901522d0849ac2d9223a2b69c0324
ringabout <43030857+ringabout@users.noreply.github.com>
2024-12-05 20:34:34 +0800

    fixes strictdefs warnings for stdlibs

--- lib/pure/colors.nim
+++ lib/pure/colors.nim
@@ -92,9 +92,7 @@ proc extractRGB*(a: Color): tuple[r, g, b: range[0..255]] =
     echo extractRGB(b)
     echo typeof(extractRGB(b))
 
-  result.r = a.int shr 16 and 0xff
-  result.g = a.int shr 8 and 0xff
-  result.b = a.int and 0xff
+  result = (a.int shr 16 and 0xff, a.int shr 8 and 0xff, a.int and 0xff)
 
 proc intensity*(a: Color, f: float): Color =
   ## Returns `a` with intensity `f`. `f` should be a float from 0.0 (completely
