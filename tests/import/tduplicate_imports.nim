b0d6faf278fa9fed316ed243932e2444a4a7a776
Jake Leahy <jake@leahy.dev>
2024-12-07 15:49:00 +1100

    Add more test cases

--- tests/import/tduplicate_imports.nim
+++ tests/import/tduplicate_imports.nim
@@ -2,8 +2,14 @@ discard """
   cmd: '''nim c --hint:Processing:off $file'''
   action: compile
   nimout: '''
-tduplicate_imports.nim(9, 23) Hint: duplicate import of 'strutils'; previous import here: tduplicate_imports.nim(9, 13) [DuplicateModuleImport]
+tduplicate_imports.nim(11, 23) Hint: duplicate import of 'strutils'; previous import here: tduplicate_imports.nim(11, 13) [DuplicateModuleImport]
+tduplicate_imports.nim(14, 20) Hint: duplicate import of 'foobar'; previous import here: tduplicate_imports.nim(13, 20) [DuplicateModuleImport]
+tduplicate_imports.nim(15, 20) Hint: duplicate import of 'strutils'; previous import here: tduplicate_imports.nim(11, 23) [DuplicateModuleImport]
 '''
 """
 
 import std/[strutils, strutils]
+
+import strutils as foobar
+import strutils as foobar
+from strutils import split
